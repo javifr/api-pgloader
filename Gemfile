8080c6bc368e47ae936d0de593e8dc67e7499b0d
Javi Fernández <jfernandezriverola@gmail.com>
2016-01-11 16:22:51 +0100

    Wow!

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+gem 'rack'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'pry'
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+end
---
+++ app.rb
@@ -0,0 +1,77 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'sinatra/json'
+require 'pry'
+
+class Api < Sinatra::Base
+
+    set :db, 'postgres://javi:@localhost:5432/loyal_guru?tablename='
+    set :path '/Users/javi/code/api-pgloader/tmp/'
+
+    get '/api' do
+      'This api is working mate!'
+    end
+
+    get '/api/load' do
+
+      # Params
+      table = params[:table] #activities
+      company = params[:company] # canada
+      url = params[:url] # http://amazon....
+      fields = params[:fields] # id,product_type,description
+
+      # Env Vars
+      db_connect = "#{settings.db}#{company}.#{table}"
+      filename = "#{company}.#{table}.#{Time.now.strftime("%Y%m%d%M%S")}.load"
+
+      # Creating load file
+      contents = "LOAD CSV\n"+
+         "FROM '#{url}'\n"+
+              "HAVING FIELDS\n"+
+              "(\n"+
+               "#{fields}\n"+
+              ")\n"+
+         "INTO #{db_connect}\n"+
+          "TARGET COLUMNS\n"+
+          "(\n"+
+          "#{fields}\n"+
+          ")\n"+
+          "WITH fields terminated by ';',\n"+
+          "skip header = 1\n"+
+      ";"
+
+      # Writing load file
+      out_file = File.new(settings.path+filename, "w")
+      out_file.puts(contents)
+      out_file.close
+
+      # Calling pgloader file.load
+      ret = `pgloader #{settings.path+filename}`
+
+      # Managing response
+      if $?.success?
+        # Write success log file
+        out_file = File.new(settings.path+filename+".log", "w")
+        out_file.puts(ret)
+        out_file.close
+
+        if ret.include? "Total import time"
+          ret = ret.split("Total import time")
+          ret = ret[1].gsub(/\s+/, ' ').split(" ")
+          # status 200 response with json content
+          json :read => ret[0], :imported => ret[1], :erros => ret[2]
+        end
+
+      else
+        # Write error log file
+        out_file = File.new(settings.path+filename+".error", "w")
+        out_file.puts(ret)
+        out_file.close
+
+        # status 500 response indicating server error
+        status 500
+      end
+
+    end
+
+end
